0cfdcf8961d13cd90811dc57e217226e4d504b45
maksius13 <maksius13@mail.ru>
2022-06-06 21:51:53 +0300

    ДЗ по работе с ошибками 2

--- src/main/java/ru/chentsov/otushw/testingsystem/errors/Example.java
+++ src/main/java/ru/chentsov/otushw/testingsystem/errors/Example.java
@@ -1,10 +1,16 @@
 package ru.chentsov.otushw.testingsystem.errors;
 
 import java.io.Closeable;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Example {
     public static void main(String[] args) {
+        for (int i = 0; i < 1_000; i++) {
+            OutOfMemory.start("Google Chrome", 100_000_000);
+            OutOfMemory.drop();
+        }
+
         Client client = new Client("Max", 25);
         try{
             client.createAccount(-100);
@@ -25,7 +31,15 @@ public class Example {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
+        Connect connect2 = new Connect();
+        try {
+            connect2.doSth();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            connect2.close();
+        }
     }
 }
 
@@ -36,11 +50,35 @@ class Connect implements Closeable {
     }
 
     void doSth() throws Exception{
-        throw new Exception("Я упал");
+        throw new MyException("Я упал");
     }
 
     @Override
     public void close() {
         System.out.println("Я закрылся");
+    }
+}
+
+class MyException extends Exception{
+    MyException(){
+        super();
+    }
+
+    MyException(String s){
+        super(s);
+    }
+}
+
+class OutOfMemory{
+    static private List<String> list = new ArrayList<>();
+
+    static void start(String s, int iteration){
+        for (int i = 0; i < iteration; i++) {
+            list.add(s + i);
+        }
+    }
+
+    static void drop(){
+        list = new ArrayList<>();
     }
 }
